3c4b7c04de449f7911e3b146a4f2b9362b96c7fb
aroig1 <aroig@arizona.edu>
2023-08-11 16:55:02 -0700

    Finished Project Got API working

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-const { generateResponse } = require('./chatgpt.js');
 const listContainer = document.getElementById("list-container");
 const inputBox = document.getElementById("input-box");
 
-function AddMessage() {
+async function chatResponse(message) {
+    const apiKey = 'API_KEY';
+    const apiURL = 'https://api.openai.com/v1/chat/completions';
+
+    const response = await fetch(apiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: "user", content: message }],
+            max_tokens: 200
+        })
+    })
+
+    const data = await response.json();
+    return data.choices[0].message.content;
+}
+
+async function AddMessage() {
     if (inputBox.value === '') {
         alert("You must input a message.");
+        inputBox.value = '';
     }
     else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         li.classList.add("user");
         listContainer.appendChild(li);
-        chatResponse(li.innerHTML);
+        inputBox.value = '';
+        let res = document.createElement("li");
+        res.innerHTML = 'loading...'
+        listContainer.appendChild(res);
+        window.scrollTo(0, window.innerHeight);
+        const response = await chatResponse(li.innerHTML);
+        res.innerHTML = response;
+        window.scrollTo(0, window.innerHeight);
     }
-    inputBox.value = '';
 }
 
-async function chatResponse(message) {
-    let response = await generateResponse(message);
-    let li = document.createElement("li");
-    li.innerHTML = response;
-    listContainer.appendChild(li);
-}
-
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keypress', function(e) {
     if (e.key === 'Enter') {
-        e.preventDefault();
         AddMessage();
     }
 });
